85e312ee49f8236fd6e30affacd4e7f61953884b
ty <tytia77@gmail.com>
2026-05-28 16:58:28 +1000

    update config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // images: {
+  //   domains: ["image.tmdb.org"],
+  // },
   images: {
-    domains: ["image.tmdb.org"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
